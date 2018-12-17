f7006ad5c111b324bbc0340ac84a19e457131dae
mattuy <mattuylee@outlook.com>
2018-12-17 21:53:52 +0800

    修改获取用户列表API

---
+++ Interact/Exception.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interact
+{
+	class ConnectionBrokenException : Exception
+	{
+		public ConnectionBrokenException(string message) : base(message) { }
+	}
+}
