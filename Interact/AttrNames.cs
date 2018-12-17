a3ebaca23478beeae0aea51a976b590b7f685072
mattuy <mattuylee@outlook.com>
2018-12-17 11:27:23 +0800

    编写向服务器请求数据的相关函数

--- Interact/AttrNames.cs
+++ Interact/AttrNames.cs
@@ -8,10 +8,14 @@ namespace Interact
 	//Json数据的字段访问字符串
 	public static class AttrNames
 	{
+		//基本信息
 		public const string Token = "Token";
 		public const string Operation = "Operation";
-		public const string Error = "Error";
 
+		//处理结果信息
+		public const string Error = "Error";
+		
+		//用户信息
 		public const string User = "User";
 		public const string NickName = "NickName";
 		public const string Motto = "Motto";
