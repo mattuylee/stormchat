fae7ce37e587fe652f93efa224bc2cd6995e4b2e
KaniuTayou <1056879522@qq.com>
2018-12-24 17:53:55 +0800

    未完成，保存

---
+++ stormchat-client-csharp/StormChatWPF/UI/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿using Interact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace StormChatWPF
+{
+    class UserInfo:Label
+    {
+        public UserInfo(User user)
+            {
+                
+                
+            }
+        private Image headpicture = new Image
+        {
+            HorizontalAlignment = HorizontalAlignment.Left
+        };
+        private TextBlock UserNmae=new TextBlock
+        {
+            FontFamily = new FontFamily("Comic Sans MS"),
+            HorizontalAlignment = HorizontalAlignment.Right,
+            VerticalAlignment=VerticalAlignment.Top
+        };
+    }
+}
